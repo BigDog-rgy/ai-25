4f99094803dbe89c636d95f1b147812c2f319389
C-DOG <cvrolson@gmail.com>
2025-06-01 00:11:50 -0500

    update supabase's path

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { supabase } from '../../lib/supabaseClient';
+import { supabase } from '../lib/supabaseClient';
 
 export default async function HomePage() {
   const { data: companies, error } = await supabase.from('companies').select('*');
